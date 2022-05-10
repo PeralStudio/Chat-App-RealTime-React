78725bc9c35248dbd6684b71692eea4ed7514e25
Alberto <info@peralstudio.com>
2022-05-10 20:35:23 +0200

    ✔️ Update

--- client/src/components/TextContainer/TextContainer.js
+++ client/src/components/TextContainer/TextContainer.js
@@ -7,28 +7,23 @@ import './TextContainer.css';
 const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
-    <h1 className="txtCol">Chat App Real-Time React <span role="img" aria-label="emoji">💬</span></h1>
-    <h4 className="created">Creado con React, Express, Node.js y Socket.IO</h4>
-
-
+      <h1 className="txtCol">Chat App Real-Time React <span role="img" aria-label="emoji">💬</span></h1>
+      <h4 className="created">Creado con React, Express, Node.js y Socket.IO</h4>
       <h5>Comparte tu nombre de sala para permitir que tus amigos se unan al chat</h5>
       <h5>"Expresa más escribiendo emoticonos que se conviertan en emojis <span role="img" aria-label="emoji">❤️"</span></h5>
       <h5>¡Pruébalo ahora mismo! <span role="img" aria-label="emoji">⬅️</span></h5>
-     
-
-      
     </div>
     {
       users
         ? (
           <div>
-           <h1 className="divider"><b> Personas conectadas:</b></h1>
+            <h1 className="divider"><b> Personas conectadas:</b></h1>
             <div className="activeContainer">
               <h4>
-                {users.map(({name}) => (
+                {users.map(({ name }) => (
                   <div key={name} className="activeItem">
                     {name}
-                    <img alt="Online Icon" src={onlineIcon}/>
+                    <img alt="Online Icon" src={onlineIcon} />
                   </div>
                 ))}
               </h4>
